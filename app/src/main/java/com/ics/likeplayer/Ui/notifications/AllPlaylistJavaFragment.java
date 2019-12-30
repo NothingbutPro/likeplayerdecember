84db9a36d19e38c746cf00b22c02c1d41931b2fd
NothingbutPro <paragsharma611@gmail.com>
2019-12-30 17:10:51 +0530

    commitlast30

---
+++ app/src/main/java/com/ics/likeplayer/Ui/notifications/AllPlaylistJavaFragment.java
@@ -0,0 +1,20 @@
+package com.ics.likeplayer.Ui.notifications;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.ics.likeplayer.R;
+
+class AllPlaylistJavaFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_allplaylist ,container,false);
+        return view;
+//        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
